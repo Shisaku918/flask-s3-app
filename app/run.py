03bcd25961beae53fb81cc32faff271ccabff5d5
Shisaku <98543783+Shisaku918@users.noreply.github.com>
2025-06-19 14:08:00 +0200

    Update run.py
    Ajout de l'initialisation de la clé secrète pour l'appli flask + import d'os pour récupérer la clé dans le fichier .env

--- app/run.py
+++ app/run.py
@@ -3,9 +3,11 @@ load_dotenv()
 
 from flask import Flask
 from app.routes import bp
+import os
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
 app.register_blueprint(bp)  # enregistre les routes
 
